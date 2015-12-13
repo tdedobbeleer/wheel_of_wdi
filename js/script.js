a3887fa3e25d1aee9e1e4e9665fe2e1ebce3f606
Chad Christensen <chadthecoder@gmail.com>
2015-12-13 14:56:45 -0500

    added play button functionality and displayed letters

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
 $(document).ready(function() {
 
+    var words = [{
+        word: "JavaScript",
+        topic: "Thing"
+    }, {
+        word: "CSS",
+        topic: "Thing"
+    }, {
+        word: "Jarrett Coger",
+        topic: "Person"
+    }]; // Close words
 
-  alert("test");
+
+    function playGame() {
+        var rand = Math.floor(Math.random() * words.length);
+        var currentWord = words[rand].word.toUpperCase();
+        console.log(typeof currentWord);
+        splitWord = currentWord.split("");
+
+        for (var i = 0; i < splitWord.length; i++) {
+            var div = $("<div></div>");
+            div.addClass("box");
+            var p = $("<p></p>");
+            p.addClass("letter")
+            p.text(splitWord[i]);
+            div.append(p);
+            $("#container").append(div);
+
+        };
+    };
+
+    $("#play").on("click", function (){
+      $(".box").remove();
+      playGame();
+    })
+
+
 
 
 });
